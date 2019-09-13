592f7c8e527cb7ceb750f50268e27c2e5341ca00
Jason Huels <jasonhuels@yahoo.com>
2019-09-13 10:54:18 -0700

    add doctor info to user interface after search

--- src/main.js
+++ src/main.js
@@ -17,16 +17,37 @@ $(function() {
       promise.then(function(response) {
         const body = JSON.parse(response);
         // first name, last name, address, phone number, website and whether or not the doctor is accepting new patients
-        let firstName = body.data[0].profile.first_name;
-        let lastName = body.data[0].profile.last_name;
-        let address = body.data[0].practices[0].visit_address.street;
-        let city = body.data[0].practices[0].visit_address.city;
-        let state = body.data[0].practices[0].visit_address.state;
-        let zip = body.data[0].practices[0].visit_address.zip;
-        let phone = body.data[0].practices[0].phones[0].number;
-        let website = body.data[0].practices[0].website;
-        let newPatients = body.data[0].practices[0].accepts_new_patients;
+        for(let i=0; i<body.data.length; i++) {
+          let firstName = body.data[i].profile.first_name;
+          let lastName = body.data[i].profile.last_name;
+          let address = body.data[i].practices[0].visit_address.street;
+          let city = body.data[i].practices[0].visit_address.city;
+          let state = body.data[i].practices[0].visit_address.state;
+          let zip = body.data[i].practices[0].visit_address.zip;
+          let phone = body.data[i].practices[0].phones[0].number;
+          let website = body.data[i].practices[0].website || "(no website available)";
+          let img = body.data[i].profile.image_url;
+          let newPatients = body.data[i].practices[0].accepts_new_patients;
+          console.log(firstName, lastName, address, city, state, zip, phone, website);
 
+          $("#results").append(`
+            <div class="card doctor row">
+              <div class="row">
+                <div class="col-md-5 personal">
+                  <img src='${img}' alt="doctor">
+                  <p>${firstName} ${lastName}<br>${website}</p>
+                </div>
+
+                <div class="col-md-5 professional">
+                  <p>${address}<br>${city}, ${state} ${zip}
+                  <p>${phone}</p>
+                  <p>${newPatients}</p>
+                </div>
+              </div>
+            </div>
+            `);
+
+        }
 
         console.log(body);
       }, function(error) {
