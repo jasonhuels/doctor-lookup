12334ec5407e006268c397478516da8b12a59b1c
Jason Huels <jasonhuels@yahoo.com>
2019-09-13 10:00:05 -0700

    setup front end to pull data from api request

--- src/main.js
+++ src/main.js
@@ -6,15 +6,38 @@ import {Search} from './search.js';
 
 $(function() {
 
-  $('#submit').click(function() {
-    let search = new Search();
-    let promise = search.findDoctor();
-    promise.then(function(response) {
-      const body = JSON.parse(response);
+  $('#search').submit(function(event) {
+    event.preventDefault();
+    const symptoms = $("#symptoms").val();
+    const name = $("#doctor-name").val();
+    const specialty = $("#specialty").val();
+    if(symptoms || name || specialty) {
+      const search = new Search();
+      const promise = search.findDoctor(symptoms, name, specialty);
+      promise.then(function(response) {
+        const body = JSON.parse(response);
+        // first name, last name, address, phone number, website and whether or not the doctor is accepting new patients
+        let firstName = body.data[0].profile.first_name;
+        let lastName = body.data[0].profile.last_name;
+        let address = body.data[0].practices[0].visit_address.street;
+        let city = body.data[0].practices[0].visit_address.city;
+        let state = body.data[0].practices[0].visit_address.state;
+        let zip = body.data[0].practices[0].visit_address.zip;
+        let phone = body.data[0].practices[0].phones[0].number;
+        let website = body.data[0].practices[0].website;
+        let newPatients = body.data[0].practices[0].accepts_new_patients;
 
-    }, function(error) {
-      $('.showErrors').text(`There was an error processing your request: ${error.message}`);
-    });
+
+        console.log(body);
+      }, function(error) {
+        $('.showErrors').text(`There was an error processing your request: ${error.message}`);
+      });
+
+    } else {
+      // NO search
+      console.log("No doctors meet the criteria.");
+    }
+
   });
 
 });
--- src/search.js
+++ src/search.js
@@ -1,5 +1,5 @@
 export class Search {
-  findDoctor(name, symptoms, specialty) {
+  findDoctor(symptoms, name, specialty) {
     return new Promise(function(resolve, reject) {
       let request = new XMLHttpRequest();
       const url = `https://api.betterdoctor.com/2016-03-01/doctors?name=${name}&query=${symptoms}&specialty_uid=${specialty}&location=or-portland&user_location=45.505,-122.675&skip=0&limit=10&user_key=${process.env.exports.apiKey}`;
